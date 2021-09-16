9b8cde23eab0770885a1802651f6aa7e96bb42e6
Caleb <51061101+calebrwalk5@users.noreply.github.com>
2021-09-15 21:03:55 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#define Main int
+#define mAin return
+#define MAin cout
+#define MAIn endl
+#define mAIn false
+#define MAIN "Hello, World"
+
+Main main() {
+	MAin << MAIN << MAIn;
+	mAin mAIn;
+}
