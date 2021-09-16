b85c4fac63e47b4acc4b0c7d0b7933dd8689f306
Caleb <51061101+calebrwalk5@users.noreply.github.com>
2021-09-15 21:01:39 -0400

    Add files via upload

---
+++ int.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+#define Int main
+#define INt cout
+#define INT endl
+#define iNt <<
+#define iNT "int"
+#define inT false
+#define InT return
+#define Main const
+#define MAin char
+#define MAIn c
+#define MAIN FILE
+#define mAin pFile
+#define mAIn fopen
+#define mAIN else
+#define maiN fclose
+#define MAlN NULL
+#define MaiN if
+#define maIn "r"
+#define INTMAIN do
+#define intmain while
+
+int Int() {
+	int MAIn;
+	MAIN * mAin;
+	mAin = mAIn ("int.cpp", maIn);
+	MaiN (mAin==MAlN) perror ("Error opening file");
+	mAIN {
+		INTMAIN {
+			MAIn = getc (mAin);
+			INt << MAIn << INT;
+		} 
+		intmain (MAIn != EOF);
+		maiN (mAin);
+	}
+	INt iNt iNT iNt INT;
+	InT inT;
+}
