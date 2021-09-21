a55e90f3e44eaa47b3c53fa998d52838bd563c7d
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-09-20 20:03:10 -0400

    Add files via upload

--- exercise3.cpp
+++ exercise3.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 const int MY = 12;
 const int WY = 52;
+const int DY= 365.25;
+const int MD = 1440;
 int eY;
 int eM;
 int cM;
@@ -16,6 +18,9 @@ int main() {
 	cin >> eY >> eM;
 	cM = (eY * MY) + eM;
 	cW = (eY * WY) + (MY * (eM / MY));
-	cout << "Months: " << cM << endl << "Weeks: " << cW << endl;
+	cD = (eY * DY) + (DY * (WY / float(eM)));
+	cM = cD * MD;
+	cH = cM / 60;
+	cout << "Months: " << cM << endl << "Weeks: " << cW << endl << "Days: " << cD << endl << "Hours: " << cH << endl << "Minutes: " << cM << endl;
 	return false;
 }
