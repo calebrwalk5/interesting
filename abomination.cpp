e7e39181c42198468f276fd40e22acdae462a44f
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-09-24 09:58:26 -0400

    Update abomination.cpp

--- abomination.cpp
+++ abomination.cpp
@@ -1,3 +1,4 @@
+/* this is an abomination */
 #include <iostream>
 using namespace std;
 #define int section
@@ -5,9 +6,12 @@ using namespace std;
 #define main _start
 #define char db
 #define return call
+#define for LOOP
 
 section _start() {
   db msg = "hello, world";
-  mov << msg << "\n";
-  call 0;
+  LOOP(section i = 0; i < 4; i++) {
+    mov << msg << "\n";
+  }
+  call false;
 }
